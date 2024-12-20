0ee2a7300b1c36e3f859c092698315f01b03de4f
Laith Malky <laith.malky@gmail.com>
2024-12-20 22:16:10 +0200

    initial commit

---
+++ day1.py
@@ -0,0 +1,26 @@
+# print in the first code from python udemy code 
+print("first python code from udemy python course\ntest " + "test")
+
+# input in the first python udemy code 
+print("Hello " + input("Whats your name? ") + "!")
+
+name = input("Whats your name? ")
+name2 = "Jack"
+print("Hello " + name + "!")
+print(len(name))
+print("Hello " + name2 + "!")
+print(len(name2))
+
+# length in first code from udemy corse 
+
+username = input("Whats your name? ")
+usernamelen = len(username)
+print("Hello " + username + "! " + str(usernamelen))
+
+
+# final project for day one
+
+print("Welcome the Band Name Generator. ")
+city = input("Whats the name of the city you grew up in? ")
+pet = input("Whats your pets name? ")
+print("Your band name could be " + pet + " " + city)
